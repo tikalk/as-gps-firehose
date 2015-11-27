5ac14a7a850b3cb2f14fbc2cd485f5edc0b7cf88
Yanai Franchi <yanai@tikalk.com>
2015-11-27 18:02:33 +0200

    first

---
+++ run-bin/load-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No profile is supplied. You need to run it as load-test.sh <profile> . Profile is used to choose the exact configuration, and current options are default and load."
+    exit -1
+fi
+
+echo "Starting Load Test App $1..."
+
+DIRNAME=`dirname $0`
+APP_HOME=`cd $DIRNAME/..;pwd;`
+export APP_HOME;
+
+java -Duser.timezone="UTC" -Dspring.profiles.active=load-app,"$1" -jar $APP_HOME/build/libs/load-test-1.0.0.jar
---
+++ src/main/java/com/tikal/angelsense/gpsFirehose/Application.java
@@ -0,0 +1,59 @@
+package com.tikal.angelsense.gpsFirehose;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.goebl.david.Response;
+import com.goebl.david.Webb;
+
+@SpringBootApplication
+public class Application {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Application.class);
+
+	@Value("${deviceManagerPostGpsUrl}")
+	private String deviceManagerPostGpsUrl;
+	
+	@Value("${schedualeIntervalInMillis}")
+	private int schedualeIntervalInMillis;
+	
+	@Value("${gpsInputFile:/var/log/angelsense/gps.log}")
+	private String gpsInputFile;
+	
+	private Webb webb;
+
+	public static void main(final String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init() {
+		webb = Webb.create();
+		return this::run;
+	}
+	
+	public void run(final String... args) throws Exception {
+		final Stream<String> linesStream = Files.lines(Paths.get("/var/log/angelsense/gps.log"));
+		Files.lines(Paths.get(gpsInputFile)).forEach(this::doSend);
+		linesStream.close();
+	}
+	
+	private void doSend(final String g) {
+		try {
+			logger.debug("Sending to device-manager pending GPS via HTTP: {}...",g);
+			final Response<Void> response = webb.post(deviceManagerPostGpsUrl).body(g).ensureSuccess().asVoid();
+			logger.debug("Got from device-manager HTTP status {} for {}",response.getStatusCode(),g);
+			Thread.sleep(schedualeIntervalInMillis);
+		} catch (final Exception e) {
+			logger.error(e.getMessage());
+		}
+	}
+}
+
+
